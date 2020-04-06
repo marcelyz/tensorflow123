a988b60f89eb9eac46e50067b50c59c11deabb17
gaoyazhe <gaoyazhe@jd.com>
2020-04-06 21:28:04 +0800

    fashion_mnist multi classfication

--- tf2.1_notes/fashion.py
+++ tf2.1_notes/fashion.py
@@ -1,22 +1,9 @@
 import tensorflow as tf
-from matplotlib import pyplot as plt
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras import Model
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# plt.imshow(x_train[0], cmap="gray")
-# plt.show()
-
-print("x_train[0]:\n", x_train[0])
-print("y_train[0]:\n", y_train[0])
-
-print("x_train.shape:\n", x_train.shape)
-print("y_train.shape:\n", y_train.shape)
-print("x_test.shape:\n", x_test.shape)
-print("y_test.shape:\n", y_test.shape)
-
+fashion = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
@@ -51,3 +38,4 @@ model.compile(optimizer="adam",
 model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1)
 
 model.summary()
+
