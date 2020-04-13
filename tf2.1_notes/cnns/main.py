2b59df01c58c51d11505464c17dd35620e27be8d
gaoyazhe <gaoyazhe@jd.com>
2020-04-14 01:13:46 +0800

    lenet5 and main

---
+++ tf2.1_notes/cnns/main.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+from baseline import Baseline
+from lenet5 import LeNet5
+
+np.set_printoptions(threshold=np.inf)
+
+# data
+fashion = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print("x_train.shape: ", x_train.shape)
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)  # 给数据增加一个维度，使数据和网络结构匹配
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+print("x_train.shape: ", x_train.shape)
+
+# model
+# model = Baseline()
+model = LeNet5()
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['sparse_categorical_accuracy'])
+
+history = model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test))
+model.summary()
+
+
+# save weights
+file = open('./weights.txt', 'w')
+for v in model.trainable_variables:
+    file.write(str(v.name) + '\n')
+    file.write(str(v.shape) + '\n')
+    file.write(str(v.numpy()) + '\n')
+file.close()
+
+
+# show
+acc = history.history['sparse_categorical_accuracy']
+val_acc = history.history['val_sparse_categorical_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.subplot(1, 2, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.show()
